3b0f8864e0b5836293f0720ef9888c9f95da55c1
harjothkhara <harjoth.khara@gmail.com>
2019-06-19 16:12:27 -0700

    more tests added for lock/unlock, open/close functionaility

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,12 +4,41 @@ import '@testing-library/react/cleanup-after-each';
 import "jest-dom/extend-expect";
 
 import Dashboard from "./Dashboard";
+import Controls from '../controls/Controls';
 
 describe("<Dashboard />", () => {
     it("renders successfully", () => {
         render(<Dashboard />);
     });
-    it("locks successfully, and only when gate is closed", () => {
+    it("gate opens and closes successfully when unlocked", () => {
+        const { getByText } = render(<Dashboard />);
+        const gate = getByText(/Close/); 
+        fireEvent.click(gate);
+        fireEvent.click(gate);
+        fireEvent.click(gate);
+        fireEvent.click(gate);
+        getByText(/Open/);
+    });
+    it("gate locks successfully", () => {
+        const { getByText } = render(<Dashboard />);
+        const lock = getByText(/Lock/);
+        const gate = getByText(/Close/);
+        fireEvent.click(gate);
+        fireEvent.click(lock);
+        getByText(/Locked/);
+        getByText(/Closed/);
+    });
+    it("gate locks and unlocks successfully", () => {
+        const { getByText } = render(<Dashboard />);
+        const lock = getByText(/Lock/);
+        const gate = getByText(/Close/);
+        fireEvent.click(gate);
+        fireEvent.click(lock);
+        fireEvent.click(lock);
+        getByText(/Unlocked/);
+        getByText(/Closed/);
+    });
+    it("locks only when the gate is closed, opens only when unlocked", () => {
         const { getByText } = render(<Dashboard />);
         const lock = getByText(/Lock/);
         const gate = getByText(/Close/);
@@ -19,5 +48,5 @@ describe("<Dashboard />", () => {
         fireEvent.click(gate);
         getByText(/Locked/);
         getByText(/Closed/);
-    });
+    })
 });
