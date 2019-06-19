85bcc2b7e7bc78bcac75b6c3898faf5dc78e4d24
harjothkhara <harjoth.khara@gmail.com>
2019-06-19 13:28:04 -0700

    notes

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -5,11 +5,16 @@ const Controls = props => {
 
   return (
     <div className="controls panel">
+      {/* button is clicked (toggleLocked function invoked, closed=true and state changes: locked=true) then display 'unlock gate' if locked is false, otherwise, display lock gate.  */}
       <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+        {/* if button is clicked, gate -> locked (true), text dynamically changes to display unlock gate, otherwise if not clicked, default state conditions remain in place of locked=false, closed=true, in this case display locked gate */}
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
+
+        {/* button is clicked (toggleClosed function invoked, locked=false and state changes: closed=true) then display open gate if closed is false, otherwise, display close gate.  */}
       <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
+      {/* if button is clicked, gate -> closed (true), text dynamically changes to display open gate, otherwise if not clicked, default state conditions remain in place of locked=false, closed=false, in this gate display close gate. */}
+        {closed ? 'Open Gate' : 'Close Gate'} 
       </button>
     </div>
   );
--- src/display/Display.js
+++ src/display/Display.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+//conditionally rendering display colors of whether gate is closed/open and locked/unlocked
 const Display = ({ closed, locked }) => {
+  // if closed is true display red, otherwise display green
   const closedClass = `led ${closed ? 'red-led' : 'green-led'}`;
   const lockedClass = `led ${locked ? 'red-led' : 'green-led'}`;
 
   return (
     <div className="display panel">
+      {/* conditionally rendering text display, know what to display based on state changes */}
       <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
+      {/* if closed is true display closed, otherwise display open */}
       <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
     </div>
   );
