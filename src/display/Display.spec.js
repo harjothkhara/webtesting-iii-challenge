4bfc6bd3e484b7aebf3a2d8e50499dd7c334b6df
harjothkhara <harjoth.khara@gmail.com>
2019-06-19 13:37:27 -0700

    added a basic render test for the remaining components

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from 'react';
+import { ReactDOM } from "react-dom";
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import "jest-dom/extend-expect";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("renders successfully", () => {
+    render(<Display />);
+  });
+});
