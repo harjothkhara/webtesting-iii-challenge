22e02b7731b9a232e49710359fef79b78dad019f
harjothkhara <harjoth.khara@gmail.com>
2019-06-19 10:18:48 -0700

    npm i, added npm i @testing-library/react -D, added basic render test for Controls component (imported render and fireEvent methods from testing library)

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import { ReactDOM } from "react-dom";
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+    it("renders successfully", () => {
+        render(<Controls />);
+    });
+});
